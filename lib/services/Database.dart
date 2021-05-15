0787eb88607ba855b4da89c9ead0f39225830cf1
nadinCK <nadin@cloudkaptan.com>
2021-05-15 10:46:09 +0530

    local changes

--- lib/services/Database.dart
+++ lib/services/Database.dart
@@ -1,8 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:sms_autofill/sms_autofill.dart';
+
 
 class DatabaseMethods {
   // getUserByUsername(String username) async {
