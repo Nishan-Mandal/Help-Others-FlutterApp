c8643c44b96367b6c97fe02f11b66ccfd66d9805
Nishan-Mandal <nishanmandal667@gmail.com>
2021-05-16 21:26:18 +0530

    add checkUserExistance

--- lib/services/Database.dart
+++ lib/services/Database.dart
@@ -1,23 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:sms_autofill/sms_autofill.dart';
 
 class DatabaseMethods {
-  // getUserByUsername(String username) async {
-  //   return await FirebaseFirestore.instance
-  //       .collection("users")
-  //       .where("name", isEqualTo: username)
-  //       .get();
-  // }
-
-  // uploadUserInfo(userMap) {
-  //   FirebaseFirestore.instance
-  //       .collection("user_account")
-  //       .add(userMap)
-  //       .catchError((e) {
-  //     print(e.toString());
-  //   });
-  // }
   uploadUserInfo(String name, String mobileNumber, String email, String photo,
       String uid) {
     FirebaseFirestore.instance
